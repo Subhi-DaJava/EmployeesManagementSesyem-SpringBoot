29095076ef5e2b33bd1f58590c9e4001fafdac8a
Subhi YD <subhy809fr@gmail.com>
2022-03-17 20:16:04 +0100

    V1 Projet initialisé

---
+++ src/main/java/net/javaguides/springboot/backed/SpringBootBackedApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot.backed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/javaguides/springboot/backed/SpringBootBackedApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot.backed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
